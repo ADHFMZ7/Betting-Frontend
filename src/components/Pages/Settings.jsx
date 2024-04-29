03fb433eb79abfac60949a8ed1135f4f1acdda38
adhfmz7 <ahmadaldasouqi559@gmail.com>
2024-04-29 03:11:11 -0700

    Revert "Styled settings"
    This reverts commit 914c493728b5b5224ec898d2d08965e2d4e25404.

--- src/components/Pages/Settings.jsx
+++ src/components/Pages/Settings.jsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import { ModeToggle } from '../ModeToggle';
 import Navbar from '../Navbar';
-import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
-  } from "@/components/ui/accordion"
-  
 
 const Settings = () => {
     return (
@@ -15,14 +8,7 @@ const Settings = () => {
             <Navbar /> 
             <div>
                 <h1>Settings</h1>
-                <Accordion type="single" collapsible>
-                    <AccordionItem value="item-1">
-                        <AccordionTrigger>Display</AccordionTrigger>
-                        <AccordionContent>
-                            <ModeToggle />
-                        </AccordionContent>
-                    </AccordionItem>
-                </Accordion>
+                <ModeToggle />
             </div>
         </>
     );
